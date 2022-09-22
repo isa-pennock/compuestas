0830d6224a467c3ca08cdc8f832f244a12866561
isa-pennock <112428957+isa-pennock@users.noreply.github.com>
2022-09-22 11:46:49 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,16 @@ input.onButtonPressed(Button.A, function () {
     T = randint(-22, 100)
     basic.showNumber(T)
     if (T >= -22 && T <= 14) {
+        basic.showString("Permanece en casa, hay fuertes nevadas.")
         basic.showString("R")
     } else if (T >= 15 && T <= 23) {
+        basic.showString("¡Toma tus mejores esquis y diviértete!")
         basic.showString("V")
     } else if (T >= 24 && T <= 32) {
+        basic.showString("Solo podrás esquiar en las partes más altas de las montañas.")
         basic.showString("A")
     } else if (T >= 33) {
-        basic.showString("apagado")
+        basic.showString("Lo siento, no es temporada para esquiar.")
+        basic.clearScreen()
     }
 })
